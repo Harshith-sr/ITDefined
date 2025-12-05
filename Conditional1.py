0a3d7c360ba9d451993d7d5f7754d0ed3ddffb0b
Harshith Kumar S <98448721+Harshith-sr@users.noreply.github.com>
2025-12-05 08:00:48 +0530

    Add files via upload

---
+++ Conditional1.py
@@ -0,0 +1,12 @@
+# x = input("Enter the value of x\n")
+
+# if int(x) >= 5:
+#     print("x is greater than 5")
+# else:
+#     print("x is not greater than 5")
+
+
+if False:
+    print("x is greater than 5")
+else:
+    print("x is not greater than 5")
