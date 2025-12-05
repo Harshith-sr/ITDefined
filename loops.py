a78f764720ebec7fd8c0fda8490165bf797c5b47
Harshith Kumar S <98448721+Harshith-sr@users.noreply.github.com>
2025-12-05 07:30:46 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,96 @@
+# countries = ["IND", "AUS", "NZ", "UAE"]
+
+# for el in countries:
+#     print(el.lower())
+
+# multiplicaion table
+
+# number = input ("enter the number\n")
+# number = int(number)
+
+# for i in range(1,11):
+#     print(f"{number} * {i} = {number * i}")
+
+
+# lst = ["Harshith Kumar S", "Lohith Kumar S", "Rathnamma M R", "ShivaKumar T H"]
+
+# for i in lst:
+#     print(lst[i].append(100))
+
+# set1 = { "IND", "AUS", "NZ", "UAE"}
+
+# for el in set1:
+#     print(el)
+
+# for el in range(10):
+#     if el == 5:
+#         print(el)
+
+#     # if el == 5:
+#         continue
+
+#     print("---->\n")
+
+# countries = ["IND", "AUS", "NZ", "UAE", "BAN", "UK", "SIN", "BHU"]
+
+# for data in countries:
+
+#     print(data)
+
+#     countries.pop()
+
+#     print(countries)
+
+#     print(id(countries))
+
+# print(countries)
+
+# print(id(countries))
+
+# countries = ["IND"]
+
+# for el in countries:
+#     print(el)
+#     countries.pop()
+
+# a = "some"
+
+# b = "some"
+
+# print(id(a))
+
+# print(id(b))
+
+# pro = "Iphone"
+
+# for products in pro:
+#     print(pro[1])
+
+# nested_loop = []
+
+# for row in range(0,3):
+#     lst = []
+
+#     for col in range(1,4):
+#             lst.append(col+row)
+# nested_loop.append(col+row)
+    
+# print(nested_loop)
+
+
+# x = True
+# counter = 0
+
+# while x:
+#     counter += 1
+#     print("Hello!!!")
+
+#     if counter == 3:
+#         x = False
+
+countries = ["IND", "PAK", "AFG", "SL", "UK", "US", "NZ"]
+
+
+while len(countries):
+    print(countries)
+    countries.pop()
---
+++ operators1.py
@@ -0,0 +1,82 @@
+# # swapping 
+# a , b = 7 , 9
+
+# print(f"Before swapping value of a ==> {a}")
+# print(f"Before swapping value of b ==> {b}")
+
+# a, b = b, a
+
+# print(f"After swapping value of a ==> {a}")
+# print(f"After swapping value of b ==> {b}")
+
+a = [200, 300]
+
+z = a.append(100)
+
+y = [200, 300]
+
+print (id(a))
+print (id(z))
+# print (id(y))
+
+print(a)
+
+print(z)
+
+
+# lst1 = ["A", "B", "C"]
+
+
+# lst2 = lst1 .copy()
+
+# lst1.append("200")
+
+# print(id(lst1[0]))
+
+# print(id(lst2[0]))
+
+# # print(id(lst1[0]))
+
+# # print(id(lst1[1]))
+
+# # print(id(lst1[2]))
+
+# print(lst1)
+
+# print(lst2)
+
+
+# a = 10
+# b = 25
+
+# print(a+15 == b)
+
+# print(a > b)
+
+# print(a < b)
+
+# print(a >= b)
+
+# print(a <= b)
+
+# print(a != b)
+
+
+# a = 100
+
+# b = "100A"
+
+# if isinstance(b , str):
+#     print(a == b)
+
+
+# marks = input("Enter the marks \n")
+
+# if int(marks) >= 90:
+#     print("Distinction")
+# elif 89<= int(marks) >=65:
+#     print("First class")
+# elif 64<= int(marks) >=45:
+#     print("Second class")
+# else:
+#     print("Failed")
